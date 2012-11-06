4d8f45b783ef62a90aa1ec7352dcd227a2241d80
Andrew Church <ahc2483@gmail.com>
2012-11-05 23:04:14 -0500

    initial project commit

---
+++ RollCall/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RollCall::Application.config.secret_token = '0b2297268ea721e422b05c7fadc796404d949ac20b9b7b851e3618cada06d55bc354076d6c65f90858eca20889045b15abeb3da51c83c158b39aa85964730bf9'
