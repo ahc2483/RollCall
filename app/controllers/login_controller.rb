4980c72572cd675649d5afd703500e69127be840
Andrew Church <ahc2483@gmail.com>
2012-11-10 16:06:07 -0500

    switched to server-side workflow

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,6 +1,11 @@
 class LoginController < ApplicationController
   def handle
     
+    @user_access_token = params[:code]
+    
+    Rails.logger.info(@user_access_token)
+    
+    
     #parse for Facebook token
     
     #If the token is valid, redirect to MyClasses controller
