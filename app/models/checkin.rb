9e0bfcc31996c581744566b580950352c1827e6a
Corey <clj89@live.com>
2012-11-10 18:37:05 -0500

    added methods

--- app/models/checkin.rb
+++ app/models/checkin.rb
@@ -1,3 +1,17 @@
 class Checkin < ActiveRecord::Base
-  # attr_accessible :title, :body
+    attr_accessible :Student, :Course, :Location
+    belongs_to :Course :include => Location
+    has_one :Student
+    
+    def new
+        
+    end
+    
+    def Checkin.Post
+        
+    end
+    
+    def saveRecord
+    
+    end
 end
