c9bc9dc7d930b66789c36596ccffb8c0329224c8
Andrew Church <ahc2483@gmail.com>
2012-11-11 18:06:13 -0500

    stubbed out a browser proxy

---
+++ app/assets/javascripts/browser.js
@@ -0,0 +1,16 @@
+
+
+var Browser = {};
+
+Browser.getCurrentLocation = function(){
+	
+	var location = {};
+	
+	//TODO: Implement logic to find location
+	
+	//location.latitude = the latitude found
+	
+	//location.longitude = the longitude found
+	
+	return location;
+}
