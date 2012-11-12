c3a1c8a40b4cf805af75bf37f6bac07d354f46d9
Andrew Church <ahc2483@gmail.com>
2012-11-12 01:24:28 -0500

    added logging line

--- app/models/checkin.rb
+++ app/models/checkin.rb
@@ -8,6 +8,8 @@ class Checkin < ActiveRecord::Base
          
       if(self.student.post_to_facebook)
         
+        Rails.logger.info("Student would like to post to facebook")
+        
         @graph = Koala::Facebook::API.new(self.student.fb_access_token)
         
         @graph.put_connections("me", "rollcallapp:check_in_to", :class => "http://rollcallapp.herokuapp.com/course_object.html")
